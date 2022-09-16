87f02234c1ffdfa938d54622beb16f28f5256da2
amarnath2780 <motorolaonefusion@gmail.com>
2022-09-16 23:39:51 +0530

    sixth

--- entrega/settings.py
+++ entrega/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-)v%_hqk6rsr!xo3l!t0oyqp-gtavi!62^(y*+=%vlfv5o(762d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
