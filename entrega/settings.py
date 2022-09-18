c5678ab2e880ecf48d6b13cd6c4bb80face61f9f
amarnath2780 <motorolaonefusion@gmail.com>
2022-09-19 04:54:45 +0530

    update at 4:54AM

--- entrega/settings.py
+++ entrega/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)v%_hqk6rsr!xo3l!t0oyqp-gtavi!62^(y*+=%vlfv5o(762d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
