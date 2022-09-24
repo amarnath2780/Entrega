30d174b3c919436809264dd22a1c0a9657355f20
Amarnath <motorolaonefusion@gmail.com>
2022-09-24 20:39:29 +0530

    updated

--- entrega/settings.py
+++ entrega/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 AUTH_USER_MODEL = 'accounts.Account'
